9ad079842fff9b59b3657ba09c9a82c96cdd3fcf
ladyalice <alicejoyk@gmail.com>
2013-10-07 12:25:54 -0700

    Rails App with Status Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackbook::Application.config.secret_token = '1bf110e542323a1186fe80a3f7e8b55ca3ac24995fc3b251f362b407b770cbab64b0d6ed9b6b94bce6dd4c5c7f9f19c1fae10fe0d9d205761211c27a4fff6868'
